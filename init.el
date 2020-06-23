0dcbd5bd2ad3e6f49e93ad851a34ec9476de7e1e
YBK <ybkamaleri@users.noreply.github.com>
2020-06-23 15:31:15 +0200

    Add R path to ess inferior

--- init.el
+++ init.el
@@ -593,38 +593,6 @@ Version 2017-09-01"
   (add-to-list 'crux-indent-sensitive-modes 'markdown-mode)
   )
 
-;; Toggle letter-case
-;; Instead of using M-u/l/c
-(defun xah-toggle-letter-case ()
-  "Toggle the letter case of current word or text selection.
-Always cycle in this order: Init Caps, ALL CAPS, all lower.
-URL `http://ergoemacs.org/emacs/modernization_upcase-word.html'
-Version 2019-11-24"
-  (interactive)
-  (let (
-        (deactivate-mark nil)
-        $p1 $p2)
-    (if (use-region-p)
-        (setq $p1 (region-beginning) $p2 (region-end))
-      (save-excursion
-        (skip-chars-backward "0-9A-Za-z")
-        (setq $p1 (point))
-        (skip-chars-forward "0-9A-Za-z")
-        (setq $p2 (point))))
-    (when (not (eq last-command this-command))
-      (put this-command 'state 0))
-    (cond
-     ((equal 0 (get this-command 'state))
-      (upcase-initials-region $p1 $p2)
-      (put this-command 'state 1))
-     ((equal 1 (get this-command 'state))
-      (upcase-region $p1 $p2)
-      (put this-command 'state 2))
-     ((equal 2 (get this-command 'state))
-      (downcase-region $p1 $p2)
-      (put this-command 'state 0)))))
-
-(bind-key "C-8" 'xah-toggle-letter-case)
 
 (use-package simple
   ;; Improvements over simple editing commands
@@ -633,8 +601,8 @@ Version 2019-11-24"
   :hook ((prog-mode) . auto-fill-mode)
   ;; resize buffer accordingly
   :bind
-  ("<f8>" . (lambda () (interactive) (progn (visual-line-mode)
-                                       (follow-mode))))
+  ("<f12> v" . (lambda () (interactive) (progn (visual-line-mode)
+                                          (follow-mode))))
   ;; M-backspace to backward-delete-word
   ;; C-S-backspace is used by sp-kill-whole-line
   ("M-S-<backspace>" . backward-kill-sentence)
@@ -647,6 +615,7 @@ Version 2019-11-24"
   ("M-l" . downcase-dwim)
   ("M-c" . capitalize-dwim)
   ("M-u" . upcase-dwim)
+  ("C-8" . xah-toggle-letter-case)
   ;; Super useful for "merging" lines together, overrides the much less
   ;; useful tab-to-tab-stop:
   ("M-i" . delete-indentation)
@@ -707,6 +676,41 @@ Otherwise, call `delete-blank-lines'."
   (setq-default visual-fill-column-width 119
                 visual-fill-column-center-text nil)
   :config
+
+  ;; Toggle letter-case
+  ;; Instead of using M-u/l/c
+  (defun xah-toggle-letter-case ()
+    "Toggle the letter case of current word or text selection.
+Always cycle in this order: Init Caps, ALL CAPS, all lower.
+URL `http://ergoemacs.org/emacs/modernization_upcase-word.html'
+Version 2019-11-24"
+    (interactive)
+    (let (
+          (deactivate-mark nil)
+          $p1 $p2)
+      (if (use-region-p)
+          (setq $p1 (region-beginning) $p2 (region-end))
+        (save-excursion
+          (skip-chars-backward "0-9A-Za-z")
+          (setq $p1 (point))
+          (skip-chars-forward "0-9A-Za-z")
+          (setq $p2 (point))))
+      (when (not (eq last-command this-command))
+        (put this-command 'state 0))
+      (cond
+       ((equal 0 (get this-command 'state))
+        (upcase-initials-region $p1 $p2)
+        (put this-command 'state 1))
+       ((equal 1 (get this-command 'state))
+        (upcase-region $p1 $p2)
+        (put this-command 'state 2))
+       ((equal 2 (get this-command 'state))
+        (downcase-region $p1 $p2)
+        (put this-command 'state 0)))))
+
+  ;; (bind-key "C-8" 'xah-toggle-letter-case)
+
+
   ;; ;; Resize windows
   ;; (defun win-resize-top-or-bot ()
   ;;   "Figure out if the current window is on top, bottom or in the
@@ -2309,8 +2313,10 @@ In that case, insert the number."
                          (eshell/alias "ll" "ls -l")
                          (eshell/alias "la" "ls -al")
                          ;;Git things
-                         (eshell/alias "gitp" "cd c:/Users/ybka/Documents/GitHub/$1")
-                         (eshell/alias "gitf" "cd c:/Users/ybka/Documents/GitFH/$1")
+                         (eshell/alias "gitp" "cd c:/Users/ybka/Documents/GitHub/$1 && ls -la")
+                         (eshell/alias "gitpp" "cd c:/Users/ybka/Documents/GitHub && ls -la")
+                         (eshell/alias "gitf" "cd c:/Users/ybka/Documents/GitFH/$1 && ls -la")
+                         (eshell/alias "gitff" "cd c:/Users/ybka/Documents/GitFH && ls -la")
                          (eshell/alias "gc" "git checkout $1")
                          (eshell/alias "gf" "git fetch $1")
                          (eshell/alias "gm" "git merge $1")
@@ -2569,7 +2575,11 @@ In that case, insert the number."
 (use-package hideshow
   :bind (("C-c TAB" . hs-toggle-hiding)
          ;; ("C-c h" . hs-toggle-hiding)
-         ("M-+" . hs-show-all))
+         ("M-+" . hs-show-all)
+         :map my-assist-map
+         ("-" . hs-toggle-hiding)
+         ("+" . hs-show-all)
+         )
   :init (add-hook #'prog-mode-hook #'hs-minor-mode)
   :diminish hs-minor-mode
   :config
@@ -2819,9 +2829,10 @@ showing them."
          )
 
   :custom
+  ;; (inferior-ess-r-program "c:/Program Files/R/R-4.0.0/bin/R.exe")
+  (inferior-ess-r-program "R")
   (ess-plain-first-buffername nil "Name first R process R:1")
   (ess-tab-complete-in-script t "TAB should complete.")
-  (inferior-R-program-name "c:/Program Files/R/R-3.6.3/bin/x64/Rterm.exe")
   (ess-style 'RStudio)
   ;; (ess-use-company t "ESS company")
   (ess-use-auto-complete 'script-only "use auto-complete instead of company")
